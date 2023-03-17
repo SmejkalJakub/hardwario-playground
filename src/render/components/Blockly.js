ebfe8a843d7d69b9732151f0c542816e0161b656
Jakub Smejkal <smejkal.jakub99@gmail.com>
2023-03-17 11:58:59 +0100

    Add possibility to deploy

--- src/render/components/Blockly.js
+++ src/render/components/Blockly.js
@@ -17,8 +17,7 @@ export default class extends Component {
 
     render() {
         return (
-            <iframe src="http://localhost:8000" width="100%" height="100%" />
+            <iframe src="http://127.0.0.1:8000" width="100%" height="100%" />
         )
     }
-
 }
